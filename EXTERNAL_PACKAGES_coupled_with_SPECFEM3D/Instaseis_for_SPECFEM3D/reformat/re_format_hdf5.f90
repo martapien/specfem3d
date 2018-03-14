f97859701b2b050f90ecad0366c1ad8115e74bca
martapien <marta.pienkowska@earth.ox.ac.uk>
2018-03-14 09:57:48 +0000

    reformat into binary files in chunks to avoid runnig out of memory

--- EXTERNAL_PACKAGES_coupled_with_SPECFEM3D/Instaseis_for_SPECFEM3D/reformat/re_format_hdf5.f90
+++ EXTERNAL_PACKAGES_coupled_with_SPECFEM3D/Instaseis_for_SPECFEM3D/reformat/re_format_hdf5.f90
@@ -30,15 +30,17 @@ program re_format_hdf5
   ! vr and tr for every time step itime, but for all rec at once
   real, allocatable :: vr(:, :), tr(:, :)
   ! Iterators
-  integer irec, itime
+  integer itime, irec, itime_min, itime_max!, irec_loc
 
+  ! helpers to iterate over 8mb chunks
+  integer s, s_mod, s_index, factor, offset_time
+  real s_float
   ! Iostatus
   integer ier
 
   !! HDF5 declarations
   ! Names (file and HDF5 objects)
   ! File name hdf5_file (path to file) declared above (read off par file)
-  !character(len=17), parameter :: spec_file = "specfem_dump.hdf5"
   character(len=5),  parameter :: grp_coords = "local"
   character(len=5),  parameter :: grp_fields = "local"
   character(len=19), parameter :: dset_velocity = "local/velocity"
@@ -56,24 +58,16 @@ program re_format_hdf5
   ! Identifiers
   integer(hid_t) :: coords_file_id         ! File identifier
   integer(hid_t) :: fields_file_id         ! File identifier
-  !integer(hid_t) :: spec_file_id           ! File identifier
   integer(hid_t) :: plist_id               ! Property list identifier
   integer(hid_t) :: plist_id2              ! Property list identifier
-  !integer(hid_t) :: plist_id3              ! Property list identifier
   integer(hid_t) :: coords_grp_id          ! Group identifier
   integer(hid_t) :: fields_grp_id          ! Group identifier
-  !integer(hid_t) :: spec_grp_id            ! Group identifier
   integer(hid_t) :: dset_v_id       ! Dataset identifier
   integer(hid_t) :: dset_s_id       ! Dataset identifier
-  !integer(hid_t) :: dset_spec_d_id  ! Dataset identifier
-  !integer(hid_t) :: dset_spec_s_id  ! Dataset identifier
   integer(hid_t) :: dset_n_id       ! Dataset identifier
   integer(hid_t) :: dspace_v_id     ! Dataspace identifier
   integer(hid_t) :: dspace_s_id     ! Dataspace identifier
-  !integer(hid_t) :: dspace_spec_d_id     ! Dataspace identifier
-  !integer(hid_t) :: dspace_spec_s_id     ! Dataspace identifier
   integer(hid_t) :: dspace_n_id     ! Dataspace identifier
-  !integer(hid_t) :: dspace_attr_id  ! Dataspace identifier
   integer(hid_t) :: mspace_v_id     ! Memspace identifier
   integer(hid_t) :: mspace_s_id     ! Memspace identifier
   integer(hid_t) :: mspace_n_id     ! Memspace identifier
@@ -91,7 +85,6 @@ program re_format_hdf5
 
   integer :: v_rank = 3, s_rank = 3, n_rank = 2            ! Dataset rank
   integer(hsize_t), dimension(3) :: v_dims, s_dims, n_dims ! Dataset dimensions
-  !integer(hsize_t), dimension(3) :: d_dimsf, s_dimsf       ! Dataset dimensions
   integer(hsize_t), dimension(1) :: attr_dims = 1          ! Attribute
                                                            ! dimensions
   integer(hsize_t), dimension(1) :: attr_nbrec_by_proc_dim ! Attribute
@@ -100,8 +93,6 @@ program re_format_hdf5
   integer(hsize_t), dimension(3) :: v_countf, s_countf ! Size of the hyperslab
   integer(hsize_t), dimension(2) :: n_countf           ! in file
 
-  !integer(hsize_t), dimension(2) :: v_countm, s_countm ! Size of the hyperslab
-                                                       ! in memory
   integer(hsize_t), dimension(3) :: offsetf            ! Hyperslab offset in f.
   integer(hsize_t), dimension(2) :: n_offsetf          ! Hyperslab offset in f.
 
@@ -141,6 +132,11 @@ program re_format_hdf5
     fields_hdf5_file = hdf5_file_compressed
     call trim_string(fields_hdf5_file)
     call trim_string(meshfem_hdf5_file)
+
+    ! MPC to do define the read here by chunk size in hdf5
+    !open(27,file='./Inputs_Instaseis_Coupling/chunking.par')
+    !read(27, *) chunk_size
+    !close(27)
   endif !! myrank == 0
 
   call mpi_bcast(nSpecfem_proc, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
@@ -148,9 +144,6 @@ program re_format_hdf5
   call mpi_bcast(TRACT_PATH, 500, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
   call mpi_bcast(fields_hdf5_file, 500, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
   call mpi_bcast(meshfem_hdf5_file, 500, MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
-
-  !write(*, *) "!!!!", myrank, meshfem_hdf5_file, len(meshfem_hdf5_file)
-  !write(*, *) "!!!!", myrank, fields_hdf5_file, len(fields_hdf5_file)
 
   allocate(nrec_by_proc(nSpecfem_proc))
   allocate(offset_by_proc(nSpecfem_proc))
@@ -210,7 +203,6 @@ program re_format_hdf5
                  n_dims, error, mspace_n_id, dspace_n_id, &
                  xfer_prp = plist_id)
 
-
   !! MPC fields output file
   if (myrank == 0) then
     write(*, *) "Opening hdf5 fields output file..."
@@ -240,53 +232,23 @@ program re_format_hdf5
   call h5aread_f(attr_ntime_id, H5T_NATIVE_INTEGER, &
                  ntime, attr_dims, error)
 
-  ! Assign dimensions for datasets to read: [icomp, itime, nbrec]
-  allocate(velocity(3, ntime, nrec_by_proc(myrank+1)))
-  allocate(stress(6, ntime, nrec_by_proc(myrank+1)))
-
-  v_dims(1) = 3
-  v_dims(2) = ntime
-  v_dims(3) = nrec_by_proc(myrank+1)
-
-  s_dims(1) = 6
-  s_dims(2) = ntime
-  s_dims(3) = nrec_by_proc(myrank+1)
-
-  v_countf = (/3, ntime, nrec_by_proc(myrank+1)/)
-  s_countf = (/6, ntime, nrec_by_proc(myrank+1)/)
-
   ! Open existing datasets /local/velocity and /local/stress
   call h5dopen_f(fields_file_id, dset_velocity, dset_v_id, error)
   call h5dopen_f(fields_file_id, dset_stress, dset_s_id, error)
+
   ! Get datasets' dataspace identifiers
   call h5dget_space_f(dset_v_id, dspace_v_id, error)
   call h5dget_space_f(dset_s_id, dspace_s_id, error)
 
-  offsetf  = (/0, 0, offset_by_proc(myrank+1)/)
+  factor = int((1024 * 1024 * 1024) / (9 * 4 * float(nrec_by_proc(myrank+1))))
+  s_float = (float(ntime) / float(factor)) + 0.5
+  s = nint(s_float) ! nb of 1 GB blocks
+  s_mod = mod(float(ntime), float(factor))
 
-  ! Select subset (hyperslab) in the dataset (in the file)
-  call h5sselect_hyperslab_f(dspace_v_id, H5S_SELECT_SET_F, &
-                             offsetf, v_countf, error)
-  call h5sselect_hyperslab_f(dspace_s_id, H5S_SELECT_SET_F, &
-                             offsetf, s_countf, error)
-  ! Create memory dataspace
-  call h5screate_simple_f(v_rank, v_dims, mspace_v_id, error)
-  call h5screate_simple_f(s_rank, s_dims, mspace_s_id, error)
-  ! Read data from hyperslab in the file into the hyperslab in memory
-  call h5pcreate_f(H5P_DATASET_XFER_F, plist_id2, error)
-  call h5pset_dxpl_mpio_f(plist_id2, H5FD_MPIO_INDEPENDENT_F, error)
+  write(*, *) "ntime: ", ntime, "factor: ", factor, "s: ", s, "s_float: ", s_float, "s_mod: ", s_mod
 
-  call h5dread_f(dset_v_id, H5T_NATIVE_REAL, velocity, &
-                 v_dims, error, mspace_v_id, dspace_v_id, &
-                 xfer_prp = plist_id2)
-  call h5dread_f(dset_s_id, H5T_NATIVE_REAL, stress, &
-                 s_dims, error, mspace_s_id, dspace_s_id, &
-                 xfer_prp = plist_id2)
+  allocate(vr(3, nrec_by_proc(myrank+1)), tr(3, nrec_by_proc(myrank+1)))
 
-  call MPI_Barrier(MPI_COMM_WORLD,ier)
-
-
-  !! MPC write the specfem binary files
   call create_name_database(prname, myrank, TRACT_PATH)
   write(*, *) "Process", myrank, "opening", TRACT_PATH
 
@@ -301,32 +263,99 @@ program re_format_hdf5
 
   write(*, *) "Process", myrank, &
    "computing tractions and writing out velocities and tractions..."
-  allocate(vr(3, nrec_by_proc(myrank+1)), tr(3, nrec_by_proc(myrank+1)))
 
-  do itime = 1, ntime
-   do irec = 1, nrec_by_proc(myrank+1)
+  do s_index = 1, s
+    if (s_index /= s) then
+        v_countf = (/3, factor, nrec_by_proc(myrank+1)/)
+        s_countf = (/6, factor, nrec_by_proc(myrank+1)/)
+        ! Assign dimensions for datasets to read: [icomp, itime, nbrec]
+        allocate(velocity(3, factor, nrec_by_proc(myrank+1)))
+        allocate(stress(6, factor, nrec_by_proc(myrank+1)))
+        v_dims(1) = 3
+        v_dims(2) = factor
+        v_dims(3) = nrec_by_proc(myrank+1)
 
-      vr(1, irec) = velocity(1, itime, irec)
-      vr(2, irec) = velocity(2, itime, irec)
-      vr(3, irec) = velocity(3, itime, irec)
+        s_dims(1) = 6
+        s_dims(2) = factor
+        s_dims(3) = nrec_by_proc(myrank+1)
+    else
+        v_countf = (/3, s_mod, nrec_by_proc(myrank+1)/)
+        s_countf = (/6, s_mod, nrec_by_proc(myrank+1)/)
+        ! Assign dimensions for datasets to read: [icomp, itime, nbrec]
+        allocate(velocity(3, s_mod, nrec_by_proc(myrank+1)))
+        allocate(stress(6, s_mod, nrec_by_proc(myrank+1)))
+        v_dims(1) = 3
+        v_dims(2) = s_mod
+        v_dims(3) = nrec_by_proc(myrank+1)
 
-      tmp1 = dble(stress(1, itime, irec)) * dble(normal(1, irec)) &
-         + dble(stress(6, itime, irec)) * dble(normal(2, irec)) &
-         + dble(stress(5, itime, irec)) * dble(normal(3, irec))
-      tr(1, irec) = real(tmp1)
-      tmp2 = dble(stress(6, itime, irec)) * dble(normal(1, irec)) &
-         + dble(stress(2, itime, irec)) * dble(normal(2, irec)) &
-         + dble(stress(4, itime, irec)) * dble(normal(3, irec))
-      tr(2, irec) = real(tmp2)
-      tmp3 = dble(stress(5, itime, irec)) * dble(normal(1, irec)) &
-         + dble(stress(4, itime, irec)) * dble(normal(2, irec)) &
-         + dble(stress(3, itime, irec)) * dble(normal(3, irec))
-      tr(3, irec) = real(tmp3)
+        s_dims(1) = 6
+        s_dims(2) = s_mod
+        s_dims(3) = nrec_by_proc(myrank+1)
+    endif
 
-   enddo !! irec
-   !! MPC vr and tr for every time step itime, but for all rec at once
-   write(28) vr, tr
-  enddo !! itime
+    offset_time = (s_index - 1) * factor
+    offsetf  = (/0, offset_time, offset_by_proc(myrank+1)/)
+
+    ! Select subset (hyperslab) in the dataset (in the file)
+    call h5sselect_hyperslab_f(dspace_v_id, H5S_SELECT_SET_F, &
+                               offsetf, v_countf, error)
+    call h5sselect_hyperslab_f(dspace_s_id, H5S_SELECT_SET_F, &
+                               offsetf, s_countf, error)
+
+    ! Create memory dataspace
+    call h5screate_simple_f(v_rank, v_dims, mspace_v_id, error)
+    call h5screate_simple_f(s_rank, s_dims, mspace_s_id, error)
+    ! Read data from hyperslab in the file into the hyperslab in memory
+    call h5pcreate_f(H5P_DATASET_XFER_F, plist_id2, error)
+    call h5pset_dxpl_mpio_f(plist_id2, H5FD_MPIO_INDEPENDENT_F, error)
+    call h5dread_f(dset_v_id, H5T_NATIVE_REAL, velocity, &
+                   v_dims, error, mspace_v_id, dspace_v_id, &
+                   xfer_prp = plist_id2)
+    call h5dread_f(dset_s_id, H5T_NATIVE_REAL, stress, &
+                   s_dims, error, mspace_s_id, dspace_s_id, &
+                   xfer_prp = plist_id2)
+
+    call h5sclose_f(mspace_v_id, error)
+    call h5sclose_f(mspace_s_id, error)
+    call h5pclose_f(plist_id2, error)
+
+    !! MPC write the specfem binary files
+    itime_min = 1
+    if (s_index == s) then
+        itime_max =  s_mod
+    endif
+    if (s_index /= s) then
+        itime_max = factor
+    endif
+    write(*, *) "itime_min: ", itime_min, "itime_max: ", itime_max
+    do itime = itime_min, itime_max
+     do irec = 1, nrec_by_proc(myrank+1)
+
+        vr(1, irec) = velocity(1, itime, irec)
+        vr(2, irec) = velocity(2, itime, irec)
+        vr(3, irec) = velocity(3, itime, irec)
+
+        tmp1 = dble(stress(1, itime, irec)) * dble(normal(1, irec)) &
+           + dble(stress(6, itime, irec)) * dble(normal(2, irec)) &
+           + dble(stress(5, itime, irec)) * dble(normal(3, irec))
+        tr(1, irec) = real(tmp1)
+        tmp2 = dble(stress(6, itime, irec)) * dble(normal(1, irec)) &
+           + dble(stress(2, itime, irec)) * dble(normal(2, irec)) &
+           + dble(stress(4, itime, irec)) * dble(normal(3, irec))
+        tr(2, irec) = real(tmp2)
+        tmp3 = dble(stress(5, itime, irec)) * dble(normal(1, irec)) &
+           + dble(stress(4, itime, irec)) * dble(normal(2, irec)) &
+           + dble(stress(3, itime, irec)) * dble(normal(3, irec))
+        tr(3, irec) = real(tmp3)
+
+     enddo !! irec
+     !! MPC vr and tr for every time step itime, but for all rec at once
+     write(28) vr, tr
+    enddo !! itime
+    deallocate(velocity)
+    deallocate(stress)
+
+  enddo !! s_index
 
   close(111)
   close(28)
@@ -344,50 +373,11 @@ program re_format_hdf5
   call h5aclose_f(attr_ntime_id, error)
   call h5sclose_f(dspace_v_id, error)
   call h5sclose_f(dspace_s_id, error)
-  call h5sclose_f(mspace_v_id, error)
-  call h5sclose_f(mspace_s_id, error)
-  call h5pclose_f(plist_id2, error)
   call h5dclose_f(dset_v_id, error)
   call h5dclose_f(dset_s_id, error)
   call h5gclose_f(fields_grp_id, error)
   ! Close the hdf5 file
   call h5fclose_f(fields_file_id, error)
-
-  !if (myrank == 0) then
-  !  write(*, *) "Preparing a hdf5 file for specfem dumps..."
-  !endif
-  !! MPC Create a file that is later going to be filled out by specfem
-  ! Setup file access property list with parallel I/O access.
-  !call h5pcreate_f(H5P_FILE_ACCESS_F, plist_id3, error)
-  !call h5pset_fapl_mpio_f(plist_id3, MPI_COMM_WORLD, MPI_INFO_NULL, error)
-  ! Create the file collectively.
-  !call h5fcreate_f(spec_file, H5F_ACC_TRUNC_F, spec_file_id, error, &
-  !                 access_prp = plist_id3)
-  !call h5pclose_f(plist_id3, error)
-  ! Create new group
-  !call h5gcreate_f(spec_file_id, grp_coords, spec_grp_id, error)
-
-  !d_dimsf(1) = 3
-  !d_dimsf(2) = ntime
-  !d_dimsf(3) = nbrec
-  !s_dimsf(1) = 6
-  !s_dimsf(2) = ntime
-  !s_dimsf(3) = nbrec
-
-  ! Create data spaces for the datasets, the dims are the entire arrays
-  !call h5screate_simple_f(v_rank, d_dimsf, dspace_spec_d_id, error)
-  !call h5screate_simple_f(s_rank, s_dimsf, dspace_spec_s_id, error)
-  ! Create datasets with default properties.
-  !call h5dcreate_f(spec_grp_id, dset_spec_d, H5T_NATIVE_REAL, &
-  !                 dspace_spec_d_id, dset_spec_d_id, error)
-  !call h5dcreate_f(spec_grp_id, dset_spec_s, H5T_NATIVE_REAL, &
-  !                 dspace_spec_s_id, dset_spec_s_id, error)
-
-  !call h5dclose_f(dset_spec_d_id, error)
-  !call h5dclose_f(dset_spec_s_id, error)
-  !call h5gclose_f(spec_grp_id, error)
-  ! Close the file.
-  !call h5fclose_f(spec_file_id, error)
 
   if (myrank == 0) then
     write(*, *) "Closing the hdf5 interface..."
